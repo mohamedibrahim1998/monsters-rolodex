77627ecb38f630769be4da0ed973a705b6608478
Mohamed Ibraim <mohamedibrahim16698@gmail.com>
2020-05-07 09:34:27 +0200

    Updates

---
+++ precache-manifest.6f6584aae67fd560da86a28f4376d9cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b812dee105adbdcf4685c6610147ae9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3716c4dba9fc194b55ef",
+    "url": "/monsters-rolodex/static/css/main.4c63dd78.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3716c4dba9fc194b55ef",
+    "url": "/monsters-rolodex/static/js/main.30201662.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
